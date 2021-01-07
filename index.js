05d1be1c0c31abef1bcd936c36f4ab8f29f0b073
Sean Burnett <seanburnett@Seans-MacBook-Pro.local>
2021-01-06 21:34:50 -0500

    Done with lecture

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log('hello world');
+const fs = require('fs');
+const server = require('http').createServer();
+
+server.on('request', (req, res) => {
+  const src = fs.createReadStream('./textFile.txt');
+  src.on('data', data => res.write(data));
+  src.on('end', () => res.end());
+});
+server.listen(5000);
