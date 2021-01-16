e3a8f7a817c3f8675d111acf988f4207245d134e
Sean Burnett <seanburnett@Seans-MacBook-Pro.local>
2021-01-16 14:47:23 -0500

    Initial commit before express refactor

--- handlers/home.js
+++ handlers/home.js
@@ -8,7 +8,7 @@ const breeds = require('../data/breeds.json');
 
 module.exports = (req, res) => {
     const pathname = url.parse(req.url).pathname;
-    console.log("[home.js 10]home pathname is ", pathname);
+    //console.log("[home.js 10]home pathname is ", pathname);
     if (pathname === '/' && req.method === 'GET') {
       // Implement the logic for showing the home html view
       let filePath = path.normalize(
@@ -37,7 +37,7 @@ module.exports = (req, res) => {
       const index = fs.createReadStream(filePath);
   
       index.on('data', (data) => {
-        console.log('Breeds are: ', breeds)
+       // console.log('Breeds are: ', breeds)
         let catBreedPlaceHolder =  breeds.map( (breed) => `<option value"${breed}">${breed}</option>`)
         let modifiedData =  data.toString().replace('{{catBreeds}}', catBreedPlaceHolder)
 
@@ -66,17 +66,44 @@ module.exports = (req, res) => {
       })
     } else if (pathname === '/cats/add-cat' && req.method === 'POST') {
   
-      const index = fs.createReadStream(filePath);
+      //const index = fs.createReadStream(filePath);
+        let form =  new formidable.IncomingForm();
+        //console.log(form);
+
+        form.parse(req, (err, fields, files) => {
+          if(err) {
+            console.log(err);
+            res.write(404);
+            res.end();
+          }
+
+          let oldPath = files.upload.path;
+          let newPath = path.normalize(path.join(__dirname, '../content/images'+ files.upload.name))
+          fs.rename(oldPath, newPath, (err) => {
+            if(err) throw err;
+            console.log('File was uploaded succesfully')
+          })
+          fs.readFile('./data/cats.json', 'utf8', (err, data) => {
+
+              let allCats = JSON.parse(data);
+              allCats.push({ id: CacheStorage.length = 1, ...fields, image: files.upload.name});
+              let json = JSON.stringify(allCats);
+              fs.writeFile('./data/cats/json', json, () => {
+                res.writeHead(202, {location: '/'});
+                res.end();
+              })
+          })
+        });
   
-      index.on('data', (data) => {
-        res.write(data);
-      });
-      index.on('end', () => {
-        res.end();
-      })
-      index.on('error', (err) => {
-        console.log(err);
-      })
+      // index.on('data', (data) => {
+      //   res.write(data);
+      // });
+      // index.on('end', () => {
+      //   res.end();
+      // })
+      // index.on('error', (err) => {
+      //   console.log(err);
+      // })
   
     } else if (pathname === '/cats/add-breed' && req.method === 'POST') {
       let formData = "";
